1a41d409ce8bd4b7c001569f0b6c6ac6d2a1be27
Nabin-web <nabin.kutu11@gmail.com>
2021-01-19 09:56:41 +0545

    UPDATED [Renamed registerteam to teams]

--- route/registerteam_route.js
+++ route/registerteam_route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Teams = require('../model/registerteam')
+const Teams = require('../model/teams')
 
 router.post('/registerteam', function(req, res){
     const teamname = req.body.team_name
