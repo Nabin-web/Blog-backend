1fcc11b67c519023588d4ae4a6051d96404f092b
Nabin-web <nabin.kutu11@gmail.com>
2021-01-19 09:58:24 +0545

    UPDATED [teams_route renamed]

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const bodyParser = require('body-parser');
 const db = require('./database/db');
-const registerteam_route = require('./route/registerteam_route');
+const registerteam_route = require('./route/teams_route');
 const app = express();
 
 app.use(express.json());
