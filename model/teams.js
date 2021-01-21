98dca0900179e7ae1aeda62237edbf0f62bd72bd
Nabin-web <nabin.kutu11@gmail.com>
2021-01-21 12:20:08 +0545

    [UPDATE] Registration table validated

--- model/teams.js
+++ model/teams.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose')
 
 const Teams = mongoose.model('Teams', {
-    team_name : {type:String},
+    team_name : {type:String, required: true},
 
-    team_captain : {type: String},
+    team_captain : {type: String, required: true},
 
-    email : {type: String},
+    email : {
+        type: String, 
+        required: true, 
+        unique: true
+    },
 
-    password: {type: String},
+    password: {type: String, required: true},
 
-    team_contact: {type: String},
+    team_contact: {type: String, required: true},
 
-    team_home_ground: {type: String},
+    team_home_ground: {type: String, required: true},
 
     team_image: {type: String}
 })
--- route/teams_route.js
+++ route/teams_route.js
@@ -1,25 +1,42 @@
 const express = require('express')
 const router = express.Router()
 const Teams = require('../model/teams')
+const {check, validationResult} = require('express-validator')
 
-router.post('/registerteam', function(req, res){
-    const teamname = req.body.team_name
-    const teamcaptian = req.body.team_captian
-    const email = req.body.email
-    const password = req.body.password
-    const confirmpassword = req.body.confirm_password
-    const teamcontact = req.body.team_contact
-    const teamhomeground = req.body.team_home_ground
-    const teamimage = req.body.team_image
+//"clientData- Postman", "Message"
+router.post('/registerteam',
+[
+    check('team_name', "Team Name required!").not().isEmpty(), 
+    check('email', "Invalid Email").isEmail(),
+    check('email', "Email required!").not().isEmpty(),
+    check('team_captain', "Team Captain required").not.isEmpty(),
+    check('password', "Password required!").not.isEmpty(),
+    check('team_contact', "Team contact required!").not.isEmpty(),
+    check('team_home_ground', "Team home ground required !").not.isEmpty()
 
-    
+], 
+function(req, res){
+    const error = validationResult(req);
+    //res.send(error.array())
+    if(error.isEmpty()){
+        const teamname = req.body.team_name
+        const teamcaptain = req.body.team_captain
+        const email = req.body.email
+        const password = req.body.password
+        const confirmpassword = req.body.confirm_password
+        const teamcontact = req.body.team_contact
+        const teamhomeground = req.body.team_home_ground
+        const teamimage = req.body.team_image
 
-    const registration_data = Teams({team_name: teamname, team_captian: teamcaptian, email: email, password: password, 
-        confirm_password:confirmpassword, team_contact: teamcontact, team_home_ground: teamhomeground, team_image: teamimage })
+        const registration_data = Teams({team_name: teamname, team_captain: teamcaptain, email: email, password: password, 
+            confirm_password:confirmpassword, team_contact: teamcontact, team_home_ground: teamhomeground, team_image: teamimage })
 
-    registration_data.save()
-    res.send("Registration successful")
-
+        registration_data.save()
+        res.send("Registration successful")
+}
+else{
+    res.send(error.array());
+}
 
 
 
